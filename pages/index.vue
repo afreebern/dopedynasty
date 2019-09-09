0a6c9332a7870319786ec9507c30b900cae8b920
Aaron Freebern <afreebern@brandxads.com>
2019-09-09 10:51:18 -0400

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        dope-dynasty
-      </h1>
-      <h2 class="subtitle">
-        dope dynasty football league
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
+  <div>
+    <h1>THIS WILL HOST ALL DOPE DYNASTY CONTENT!</h1>
   </div>
 </template>
 
