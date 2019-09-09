9902fbc9e9cc7b133de8ab8fe733d8d0b517c850
Aaron Freebern <afreebern@brandxads.com>
2019-09-09 10:13:29 -0400

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,21 @@ module.exports = {
     'prettier',
     'prettier/vue'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'prefer-const': 'warn',
+    'vue/no-unused-components': 'warn',
+    'dot-notation': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        htmlWhitespaceSensitivity: 'css'
+      }
+    ]
   }
 }
