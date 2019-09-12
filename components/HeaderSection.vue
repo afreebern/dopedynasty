0f82204a6a5b9280e6e7e8921540bc6a15456a58
Aaron Freebern <afreebern@brandxads.com>
2019-09-12 13:34:35 -0400

    playsinline

--- components/HeaderSection.vue
+++ components/HeaderSection.vue
@@ -12,6 +12,7 @@
           ref="video"
           src="https://res.cloudinary.com/dipgyyv6a/video/upload/v1568135123/Aaron/y2mate.com_-_nfl_pump_up_2019_20_colors_z17ykigyH6o_920p_g2xga6.mp4"
           autoplay
+          playsinline
           muted
           loop
         ></video>
