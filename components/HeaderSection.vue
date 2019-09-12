272bd8250b3c4b18d20d4f7334ca82b739d30a65
Aaron Freebern <afreebern@brandxads.com>
2019-09-12 11:49:22 -0400

    rank box finished

--- components/HeaderSection.vue
+++ components/HeaderSection.vue
@@ -27,6 +27,86 @@
                 <h4>HOF</h4>
               </div>
             </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#2</span>
+              </div>
+              <div class="team-card drew">
+                <h4>G.O.A.T</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#3</span>
+              </div>
+              <div class="team-card david">
+                <h4>Detroit Smash</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#4</span>
+              </div>
+              <div class="team-card josh">
+                <h4>Diggity Dogg</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#5</span>
+              </div>
+              <div class="team-card aaron">
+                <h4>Fugly Mountain Trolls</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#6</span>
+              </div>
+              <div class="team-card matt">
+                <h4>Kamara Sutra</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#7</span>
+              </div>
+              <div class="team-card tyler">
+                <h4>Registered Flex Offenders</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#8</span>
+              </div>
+              <div class="team-card gio">
+                <h4>Fuck Trump</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#9</span>
+              </div>
+              <div class="team-card lindsey">
+                <h4>Kung Suh Panda</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#10</span>
+              </div>
+              <div class="team-card john">
+                <h4>Mr Papa Giorgio</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="rank-number">
+                <span>#11</span>
+              </div>
+              <div class="team-card joey">
+                <h4>Christian Mingle</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -89,7 +169,6 @@ img {
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
-  height: 1000px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -112,7 +191,7 @@ img {
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 40px;
+  font-size: 28px;
   font-weight: 700;
   span {
     color: #fff;
@@ -127,7 +206,8 @@ img {
   align-items: center;
   h4 {
     color: #fff;
-    font-size: 50px;
+    font-size: 15px;
+    text-align: center;
     letter-spacing: 7px;
     text-shadow: 2px 2px 2px 4px #000;
   }
@@ -135,6 +215,59 @@ img {
 
 .adam {
   background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568246217/Aaron/adam-card_xvpiag.jpg');
+}
+
+.drew {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568298143/Aaron/drew_y7rlge.jpg');
+}
+
+.david {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568298734/Aaron/david_dkcawy.jpg');
+}
+
+.josh {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568299393/Aaron/josh_cimn1d.jpg');
+}
+
+.aaron {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568300039/Aaron/aaron_kesdwz.jpg');
+}
+
+.matt {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568301072/Aaron/matt_ieasog.jpg');
+}
+
+.tyler {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568301430/Aaron/tp_m7l9s4.jpg');
+}
+
+.gio {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568302178/Aaron/gio_qcugum.jpg');
+}
+
+.lindsey {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568302443/Aaron/lindsey_n23mzp.jpg');
+}
+
+.john {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568302688/Aaron/john_fvt8ts.jpg');
+}
+
+.joey {
+  background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1568303156/Aaron/joey_s8v8hs.jpg');
+}
+
+@media only screen and (min-width: 550px) {
+  .team-card {
+    h4 {
+      font-size: 35px;
+    }
+  }
+  .rank-number {
+    span {
+      font-size: 40px;
+    }
+  }
 }
 
 @media only screen and (min-width: 1100px) {
@@ -151,6 +284,11 @@ img {
     position: absolute;
     top: 180px;
     z-index: 5;
+  }
+  .team-card {
+    h4 {
+      font-size: 50px;
+    }
   }
 }
 </style>
