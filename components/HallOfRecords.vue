dade3d8978dfbdd76b9a0f65ad6284440bc52a62
Aaron Freebern <afreebern@brandxads.com>
2019-09-12 19:07:07 -0400

    started hall of records

---
+++ components/HallOfRecords.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div class="hall-container">
+      <div class="winners"></div>
+      <div class="losers"></div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.hall-container {
+  width: 100%;
+  max-width: 1500px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 2000px;
+  background-color: FFF;
+}
+
+.winners,
+.losers {
+  width: 45%;
+  outline: 1px solid ORANGE;
+  height: 2000px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <header-section />
     <power-rankings />
+    <hall-of-records />
   </div>
 </template>
 
 <script>
 import HeaderSection from '~/components/HeaderSection.vue'
 import PowerRankings from '~/components/PowerRankings.vue'
+import HallOfRecords from '~/components/HallOfRecords.vue'
 
 export default {
   components: {
     HeaderSection,
-    PowerRankings
+    PowerRankings,
+    HallOfRecords
   }
 }
 </script>
